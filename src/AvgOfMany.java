0dab0561621cfc52a50fd44eeb5ad9b1e84d431a
efftushkin <efftushkin@ukr.net>
2020-05-06 19:26:48 +0300

    Final version for now

--- src/AvgOfMany.java
+++ src/AvgOfMany.java
@@ -2,20 +2,22 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Scanner;
 
-public class AvgOfTwo {
+public class AvgOfMany {
     public static void main(String[] args) {
-        System.out.println("AvgOfTwo: Welcome!");
+        System.out.println("AvgOfMany: Welcome!");
 
         Scanner scanner = new Scanner(System.in);
 
         BigDecimal num1 = null;
-        BigDecimal num2 = null;
+        BigDecimal num2;
+        int counter = 0;
 
         do {
             System.out.println("Please input the first number");
             if (scanner.hasNextBigDecimal()) {
                 num1 = scanner.nextBigDecimal();
                 System.out.println("Your input " + num1.toPlainString() + " as the first number");
+                counter++;
             } else {
                 System.out.println("Incorrect entry, exception " + scanner.next() + ". Please enter numbers. Try again");
             }
@@ -23,17 +25,19 @@ public class AvgOfTwo {
         while (num1 == null);
 
         do {
-            System.out.println("Please input the second number");
+            System.out.println("Please input the next number (to stop input not number)");
             if (scanner.hasNextBigDecimal()) {
                 num2 = scanner.nextBigDecimal();
-                System.out.println("Your input " + num2.toPlainString() + " as the second number");
+                System.out.println("Your input " + num2.toPlainString() + " as the next number");
+                num1 = num1.add(num2);
+                counter++;
             } else {
-                System.out.println("Incorrect entry, exception " + scanner.next() + ". Please enter numbers. Try again");
+                break;
             }
         }
-        while (num2 == null);
+        while (true);
 
-        System.out.println("Result: " + num1.add(num2).divide(BigDecimal.valueOf(2), 8, RoundingMode.HALF_UP).toPlainString());
+        System.out.println("Result: " + num1.divide(BigDecimal.valueOf(counter), 8, RoundingMode.HALF_UP).toPlainString());
 
         scanner.close();
         System.exit(0);
