36de15e7420e0b8cd514cdf57a680e53cf25ace0
efftushkin <efftushkin@ukr.net>
2020-05-26 17:40:22 +0300

    Final version for now

---
+++ src/MyCollectionImplementing/Main.java
@@ -0,0 +1,79 @@
+package MyCollectionImplementing;
+
+import MyCollection.MyCollection;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        MyCollectionImplementing myCollectionImplementing = new MyCollectionImplementing();
+
+        System.out.println("myCollection contains 8: " + myCollectionImplementing.contains(8));
+        System.out.println(myCollectionImplementing.toString());
+        System.out.println("myCollection is empty: " + myCollectionImplementing.isEmpty());
+
+        myCollectionImplementing.add("First");
+        myCollectionImplementing.add(2);
+        myCollectionImplementing.add(3.3);
+        myCollectionImplementing.add("Thursday");
+        myCollectionImplementing.add(new BigDecimal("5.55555"));
+
+        System.out.println(myCollectionImplementing.toString());
+        System.out.println("myCollection is empty: " + myCollectionImplementing.isEmpty());
+
+        System.out.println("Remove null: " + myCollectionImplementing.remove(null));
+        System.out.println("Remove Second: " + myCollectionImplementing.remove("Second"));
+        System.out.println("Remove 3.3: " + myCollectionImplementing.remove(3.3));
+
+        System.out.println(myCollectionImplementing.toString());
+        System.out.println("Size of myCollection: " + myCollectionImplementing.size());
+
+        myCollectionImplementing.add("Wednesday");
+        myCollectionImplementing.add("Wednesday");
+        myCollectionImplementing.add("Wednesday");
+        myCollectionImplementing.add("Wednesday");
+        myCollectionImplementing.add("Wednesday");
+        System.out.println(myCollectionImplementing.toString());
+        System.out.println("Size of myCollection: " + myCollectionImplementing.size());
+
+        System.out.println("Remove Wednesday: " + myCollectionImplementing.remove("Wednesday"));
+        System.out.println(myCollectionImplementing.toString());
+
+        System.out.println("myCollection contains 8: " + myCollectionImplementing.contains(8));
+        System.out.println("myCollection contains BigDecimal 5.55555: " + myCollectionImplementing.contains(new BigDecimal("5.55555")));
+
+        myCollectionImplementing.clear();
+        System.out.println("Clear: " + myCollectionImplementing.toString());
+        System.out.println("Size of myCollection: " + myCollectionImplementing.size());
+
+        myCollectionImplementing.add(1);
+        myCollectionImplementing.add(2);
+        myCollectionImplementing.add(3);
+        myCollectionImplementing.add(4);
+        myCollectionImplementing.add(5);
+
+        MyCollectionImplementing newMyCollectionImplementing = new MyCollectionImplementing();
+        newMyCollectionImplementing.add(3);
+        newMyCollectionImplementing.add(2);
+
+        myCollectionImplementing.retainAll(newMyCollectionImplementing);
+        System.out.println("Retain: 3, 2 of 1, 2, 3, 4, 5: " + myCollectionImplementing.toString());
+        System.out.println("Size of myCollection: " + myCollectionImplementing.size());
+
+        myCollectionImplementing.add(4);
+        myCollectionImplementing.add(5);
+        myCollectionImplementing.add(1);
+        System.out.println("Added 4, 5, 1: " + myCollectionImplementing.toString());
+
+        myCollectionImplementing.removeAll(newMyCollectionImplementing);
+        System.out.println("Removed 3, 2: " + myCollectionImplementing.toString());
+
+        System.out.println("Contains 2 and 3: " + myCollectionImplementing.containsAll(newMyCollectionImplementing));
+        myCollectionImplementing.add(3);
+        System.out.println("Added 3: " + myCollectionImplementing.toString());
+        System.out.println("Contains 2 and 3: " + myCollectionImplementing.containsAll(newMyCollectionImplementing));
+        myCollectionImplementing.add(2);
+        System.out.println("Added 2: " + myCollectionImplementing.toString());
+        System.out.println("Contains 2 and 3: " + myCollectionImplementing.containsAll(newMyCollectionImplementing));
+    }
+}
