9af1b17dce791e8ccdf7ab2edd7224f57e9287c7
efftushkin <efftushkin@ukr.net>
2020-08-02 22:29:09 +0300

    Final version for now

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,9 +1,7 @@
-import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
-import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
     @Override
