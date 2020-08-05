c9698fc082fe7ed1a42188381b2fa3c4dd3fc289
efftushkin <efftushkin@ukr.net>
2020-08-05 19:02:03 +0300

    Final version for now

--- src/ServletGetPost/ServletGetPost.java
+++ src/ServletGetPost/ServletGetPost.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Enumeration;
 
 public class ServletGetPost extends HttpServlet {
